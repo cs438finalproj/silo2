a820a81f4e4e60ba6f5d2f1171b31066e1b8ad2b
Evan Hellmuth <evan.hellmuth@gmail.com>
2016-04-26 21:45:59 -0400

    Build out PatientBank benchmarks.

--- txn/txn_types.h
+++ txn/txn_types.h
@@ -9,6 +9,7 @@
 #include <string>
 
 #include "txn/txn.h"
+#include <cstdio>
 
 // Immediately commits.
 class Noop : public Txn {
@@ -89,6 +90,48 @@ class RMW : public Txn {
     writeset_ = writeset;
   }
 
+  // ADDED FOR SILO/ADVANCED LOCKING BENCHMARKING
+  // Constructor with randomized readset/writesets in specified range which represents a single database table
+  // (begin_range inclusive, end_range exclusive)
+  // note: end_range - begin_range = table_size
+  RMW(int begin_ranges[], int end_ranges[], int num_tables, int readsetsizes[], 
+      int writesetsizes[], double time = 0) {
+    for (int i = 0; i < num_tables; ++i) {
+      int begin_range = begin_ranges[i];
+      int end_range = end_ranges[i];
+      int readsetsize = readsetsizes[i];
+      int writesetsize = writesetsizes[i];
+
+      // Make sure range is sensible
+
+      // @debug
+      //std::printf("begin = %d, end = %d, i = %d", begin_range, end_range, i);
+      
+      DCHECK(end_range > begin_range);
+
+      // Make sure we can find enough unique keys.
+      DCHECK((end_range - begin_range) >= readsetsize + writesetsize);
+
+      // Find readsetsize unique read keys.
+      for (int i = 0; i < readsetsize; i++) {
+        Key key;
+        do {
+          key = (rand() % (end_range - begin_range)) + begin_range;
+        } while (readset_.count(key));
+        readset_.insert(key);
+      }
+
+      // Find writesetsize unique write keys.
+      for (int i = 0; i < writesetsize; i++) {
+        Key key;
+        do {
+          key = (rand() % (end_range - begin_range)) + begin_range;
+        } while (readset_.count(key) || writeset_.count(key));
+        writeset_.insert(key);
+      }
+    }
+  }
+
   // Constructor with randomized read/write sets
   RMW(int dbsize, int readsetsize, int writesetsize, double time = 0)
       : time_(time) {
@@ -147,6 +190,29 @@ class RMW : public Txn {
     
     COMMIT;
   }
+
+/*
+  class Table : public RMW {
+  public:
+    Table(int begin, int end) {
+      this->begin_range = begin;
+      this->end_range = end;
+    }
+
+    int begin() {
+      return this->begin_range;
+    }
+
+    int end() {
+      return this->end_range;
+    }
+
+  private:
+    int begin_range;
+    int end_range;
+
+  };
+*/
 
  private:
   double time_;
